120f5a27e316b09dcb1f4186417a0b28a433abf7
Saif Ul Islam <saifulislam84210@gmail.com>
2021-11-12 12:29:53 +0500

    Removed ASP.NET 4.X Web App
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- SecurWebApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ExpiringBarcode.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- SecurWebApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ExpiringBarcode.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
